fc1a86f9abbe8b6a70896cc368263fd591deddbd
Jonathan Mantello <Jonathan.Mantello@Envestnet.com>
2022-07-12 16:49:03 -0700

    Added DbContext, Inital Db Migration, and Data Seeder

---
+++ Special-Needs-Analysis-Calculator.Data/SpecialNeedsAnalysisDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Special_Needs_Analysis_Calculator.Data
+{
+    public class SpecialNeedsAnalysisDbContext : DbContext
+    {
+        public SpecialNeedsAnalysisDbContext(DbContextOptions<SpecialNeedsAnalysisDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
