daecf68ccde1923e7e55d60d9e78f021ce16654d
Jacob Eul <jeul@moneyguide.com>
2022-07-13 09:45:28 -0700

    added basic implementation of User Model

---
+++ Special-Needs-Analysis-Calculator.Data/IModels/IContactInfoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Special_Needs_Analysis_Calculator.Data.IModels
+{
+    public interface IContactInfoModel
+    {
+        public void UpdateContacts(string username, string argument, string info);
+    }
+}
---
+++ Special-Needs-Analysis-Calculator.Data/IModels/IUserModel.cs
@@ -0,0 +1,16 @@
+﻿using Special_Needs_Analysis_Calculator.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Special_Needs_Analysis_Calculator.Data.IModels
+{
+    public interface IUserModel
+    {
+        public void CreateUser(string Username, string Password, ContactInfoModel ContactInfo);
+        public void UpdateUserInfo(string username, string argument, string info);
+        public void DeleteUser(string Username);
+    }
+}
