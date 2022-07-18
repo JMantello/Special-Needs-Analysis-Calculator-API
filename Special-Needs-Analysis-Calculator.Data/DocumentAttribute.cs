74b462de9657a8f3aaf3100b6764d89b678d0e15
Jonathan Mantello <Jonathan.Mantello@Envestnet.com>
2022-07-18 10:52:59 -0700

    Delete Document Attribute

--- Special-Needs-Analysis-Calculator.Data/DocumentAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Special_Needs_Analysis_Calculator.Data
-{
-    public class DocumentAttribute : Attribute
-    {
-        public int Id { get; set; }
-        public string ColumnName { get; set; }
-        public DocumentAttribute() { }
-    }
-}
