ddd726766c21a04f1642d2f4d897e28357cdfcd4
Jonathan Mantello <Jonathan.Mantello@Envestnet.com>
2022-08-03 14:28:51 -0700

    Added AbleAccountValues (Pre-Tax values), SavingsAccountValues, TaxFilingStatus enum.
    Worked on but did not complete PostTaxCapitalValues in calculator.

---
+++ Special-Needs-Analysis-Calculator.Data/Models/Person/TaxFilingStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Special_Needs_Analysis_Calculator.Data.Models.Person
+{
+    public enum TaxFilingSatus
+    {
+        Single,
+        MarriedJointly,
+        MarriedSeperately,
+        HeadOfHousehold
+    }
+}
